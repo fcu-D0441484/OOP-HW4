967160a97dfd17a7df52a50b1e7d4b5a1a1542c8
子塵 <@192.168.1.110>
2016-10-21 10:51:38 +0800

    2

--- src/fcu/iecs/oop/tiida/Main.java
+++ src/fcu/iecs/oop/tiida/Main.java
@@ -1,5 +1,22 @@
 package fcu.iecs.oop.tiida;
 
+import java.util.Scanner;
+
 public class Main {
+	public static void main(String[] args) {
+		int number;
+		int call;
+		NissanTiida b=new NissanTiida();
+		System.out.println("Please enter a number");
+		number=new Scanner(System.in).nextInt();
+		for(call=1;call<=number;call++)
+		{
+			System.out.printf("²Ä%d¦¸©I¥s\n",call);
+			b.NissanTiida(call);
+			System.out.printf("\n");
+		}
+		
+	}
+	
 
 }
